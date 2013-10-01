c5a84148bddfb8655badb515b3c848cd7c87886e
Arnauld Loyer <arnauld.loyer@gmail.com>
2013-10-01 23:10:40 +0200

    Include Cowboy skeleton

---
+++ src/erlevator.erl
@@ -0,0 +1,9 @@
+-module(erlevator).
+
+-export([start/0]).
+
+start() ->
+    ok = application:start(crypto),
+    ok = application:start(ranch),
+    ok = application:start(cowboy),
+    ok = application:start(erlevator).
