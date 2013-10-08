e9d4688f6c62265a4c93fe2f9134f83839d9bca9
Arnauld Loyer <arnauld.loyer@gmail.com>
2013-10-08 09:56:17 +0200

    Refactoring: extract handle case of into dedicated function

--- src/erlevator_handler.erl
+++ src/erlevator_handler.erl
@@ -17,64 +17,95 @@ handle(Req, []) ->
   {Method, Req1} = cowboy_req:method(Req),
   {Path,   Req2} = cowboy_req:path(Req1),
   % io:format("Method: ~p // ~p  ~n", [Method, Path]),
-  {ok, Req0} = case [Method, Path] of
-                  [<<"GET">>, <<"/nextCommand">>] ->
-                      NewCmd = erlevator_ia:next_command(),
-                      Body = command_to_body(NewCmd),
-                      cowboy_req:reply(200, [], Body, Req2);
-
-                  [<<"GET">>, <<"/reset">>] ->
-                      {Cause, Req3} = cowboy_req:qs_val(<<"cause">>, Req2),
-                      erlevator_ia:event(reset, [Cause]),
-                      cowboy_req:reply(200, Req3);
-
-                  [<<"GET">>, <<"/call">>] ->
-                      {AtFloor,   Req3} = cowboy_req:qs_val(<<"atFloor">>, Req2),
-                      {Direction, Req4} = cowboy_req:qs_val(<<"to">>, Req3),
-                      try
-                          Int = binary_to_integer(AtFloor),
-                          Dir = case Direction of
-                                  <<"UP">> -> up;
-                                         _ -> down
-                                end,
-                          erlevator_ia:event(call, [Int, Dir]),
-                          cowboy_req:reply(200, Req4)
-                      catch
-                        Ex:Term ->
-                          io:format("Ouch 'call/': ~p ~n", [Ex]),
-                          cowboy_req:reply(400, Req4)
-                      end;
-
-                  [<<"GET">>, <<"/go">>] ->
-                      {Destination, Req3} = cowboy_req:qs_val(<<"floorToGo">>, Req2),
-                      try
-                          Int = binary_to_integer(Destination),
-                          erlevator_ia:event(go, [Int]),
-                          cowboy_req:reply(200, Req3)
-                      catch
-                        Ex:Term ->
-                          io:format("Ouch 'go/': ~p ~n", [Ex]),
-                          cowboy_req:reply(400, Req3)
-                      end;
-
-                  [<<"GET">>, <<"/userHasEntered">>] ->
-                      erlevator_ia:event(user_entered, []),
-                      cowboy_req:reply(200, Req2);
-
-                  [<<"GET">>, <<"/userHasExited">>] ->
-                      erlevator_ia:event(user_exited, []),
-                      cowboy_req:reply(200, Req2);
-
-                  _ ->
-                      %% Method not allowed.
-                      cowboy_req:reply(405, Req2)
-                end,
+  {ok, Req0} = handle0(Method, Path, Req2),
   io:format("Elevator: ~p ~n", [erlevator_ia:state()]),
   {ok, Req0, undefined}.
 
 
 terminate(_Reason, _Req, _State) ->
     ok.
+
+%% ===================================================================
+%% Handle functions
+%% ===================================================================
+
+%%
+%% nextCommand
+%%
+handle0(<<"GET">>, <<"/nextCommand">>, Req2) ->
+  NewCmd = erlevator_ia:next_command(),
+  Body = command_to_body(NewCmd),
+  cowboy_req:reply(200, [], Body, Req2);
+
+%%
+%% reset event
+%%
+handle0(<<"GET">>, <<"/reset">>, Req2) ->
+  {Cause, Req3} = cowboy_req:qs_val(<<"cause">>, Req2),
+  erlevator_ia:event(reset, [Cause]),
+  cowboy_req:reply(200, Req3);
+
+%%
+%% call event
+%%
+handle0(<<"GET">>, <<"/call">>, Req2) ->
+  {AtFloor,   Req3} = cowboy_req:qs_val(<<"atFloor">>, Req2),
+  {Direction, Req4} = cowboy_req:qs_val(<<"to">>, Req3),
+  try
+    Int = binary_to_integer(AtFloor),
+    Dir = case Direction of
+            <<"UP">> -> up;
+            _ -> down
+          end,
+    erlevator_ia:event(call, [Int, Dir]),
+    cowboy_req:reply(200, Req4)
+  catch
+    Ex:Term ->
+      io:format("Ouch 'call/': ~p:~p ~n", [Ex, Term]),
+      cowboy_req:reply(400, Req4)
+  end;
+
+%%
+%% go event
+%%
+handle0(<<"GET">>, <<"/go">>, Req2) ->
+  {Destination, Req3} = cowboy_req:qs_val(<<"floorToGo">>, Req2),
+  try
+    Int = binary_to_integer(Destination),
+    erlevator_ia:event(go, [Int]),
+    cowboy_req:reply(200, Req3)
+  catch
+    Ex:Term ->
+      io:format("Ouch 'go/': ~p:~p ~n", [Ex, Term]),
+      cowboy_req:reply(400, Req3)
+  end;
+
+%%
+%% user entered event
+%%
+handle0(<<"GET">>, <<"/userHasEntered">>, Req2) ->
+  erlevator_ia:event(user_entered, []),
+  cowboy_req:reply(200, Req2);
+
+%%
+%% user exited event
+%%
+handle0(<<"GET">>, <<"/userHasExited">>, Req2) ->
+  erlevator_ia:event(user_exited, []),
+  cowboy_req:reply(200, Req2);
+
+%%
+%% All Other GET cases
+%%
+handle0(<<"GET">>, _, Req2) ->
+  cowboy_req:reply(404, Req2);
+
+%%
+%% Any other Method cases
+%%
+handle0(_, _, Req2) ->
+  %% Method not allowed.
+  cowboy_req:reply(405, Req2).
 
 %% ===================================================================
 %% Internal functions
